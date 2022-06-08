afcc27e2ef9acfde05ca0013aba14bedd3f2f7cb
Motahard <mas280499@gmail.com>
2022-06-08 15:54:21 +0300

    some changes testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import axios from 'axios';
 import React, { useEffect } from 'react'
 import './App.css';
+import { generateOpenWeatherLink, generateStormObject } from './config';
 
 const App = () => {
   useEffect(() => {
       navigator.geolocation.getCurrentPosition((pos) => {
       const { latitude, longitude } = pos.coords;
       console.log(latitude, longitude)
+      axios.get(generateOpenWeatherLink(latitude, longitude)).then(res => console.log(res.data))
+      const { link, headers } = generateStormObject(latitude, longitude);
+      axios.get(link, {
+        headers
+      }).then(res => console.log(res.data))
     });
   }, []);
   
--- src/config.js
+++ src/config.js
@@ -1,15 +1,22 @@
+import moment from "moment";
+
 export const stormGlassKey = 'b3c36daa-e70e-11ec-8af0-0242ac130002-b3c36e22-e70e-11ec-8af0-0242ac130002';
 export const openWeatherKey = '5676b04be3ead73f490dc4497ce17fbe';
 
+const firstDay = moment().startOf('day').format('x');
+const today = new Date();
+const lastDay = new Date(today.getFullYear(), today.getMonth(), today.getDate()+6);
+
+console.log(firstDay, +lastDay)
+
 export const generateOpenWeatherLink = (lat, lon) => {
-    const link = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherKey}`;
+    const link = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&&appid=${openWeatherKey}`;
     return link;
 }
 
 export const generateStormObject = (lat, lon) => {
     return {
-        params: 'windSpeed',
-        link: `https://api.stormglass.io/v1/weather/point?lat=${lat}&lng=${lon}&params=${params}`,
+        link: `https://api.stormglass.io/v1/weather/point?lat=${lat}&lng=${lon}&params=airTemperature`,
         headers: {
             'Authorization': stormGlassKey
         }
